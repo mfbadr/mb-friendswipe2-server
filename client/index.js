cf0ee687db891c77e073e0c107017c8b5e943be8
Mikey Badr <mfbadr@gmail.com>
2014-12-26 11:00:53 -0800

    remove client side

--- client/index.js
+++ client/index.js
@@ -6,12 +6,12 @@
       $urlRouterProvider.otherwise('/');
 
       $stateProvider
-        .state('home',         {url:'/',         templateUrl:'/views/home/home.html'})
-        .state('register',     {url:'/register', templateUrl:'/views/users/users.html',        controller:'UsersCtrl'})
-        .state('login',        {url:'/login',    templateUrl:'/views/users/users.html',        controller:'UsersCtrl'})
-        .state('notes',        {url:'/notes',    templateUrl:'/views/notes/notes.html',        abstract:true})
-        .state('notes.list',   {url:'?tag&page', templateUrl:'/views/notes/notes_list.html',   controller:'NotesListCtrl'})
-        .state('notes.detail', {url:'/{noteId}', templateUrl:'/views/notes/notes_detail.html', controller:'NotesDetailCtrl'});
+        .state('home',         {url:'/',         templateUrl:'/views/home/home.html'});
+        //.state('register',     {url:'/register', templateUrl:'/views/users/users.html',        controller:'UsersCtrl'})
+        //.state('login',        {url:'/login',    templateUrl:'/views/users/users.html',        controller:'UsersCtrl'})
+        //.state('notes',        {url:'/notes',    templateUrl:'/views/notes/notes.html',        abstract:true})
+        //.state('notes.list',   {url:'?tag&page', templateUrl:'/views/notes/notes_list.html',   controller:'NotesListCtrl'})
+        //.state('notes.detail', {url:'/{noteId}', templateUrl:'/views/notes/notes_detail.html', controller:'NotesDetailCtrl'});
       }])
     .run(['$rootScope', '$http', function($rootScope, $http){
       $http.get('/status').then(function(response){
