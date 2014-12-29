f653673f0fdc792898e0b0a72a4d3928842621ff
Mikey Badr <mfbadr@gmail.com>
2014-12-29 10:17:37 -0800

    sql for swipes, more test data

--- db/tables/swipes.sql
+++ db/tables/swipes.sql
@@ -1,5 +1,7 @@
 create table swipes(
+  id serial primary key,
   sender bigint not null references users(id),
   target bigint not null references users(id),
+  time timestamp not null default now(),
   liked boolean not null default false
 );
--- db/test/populate.sql
+++ db/test/populate.sql
@@ -1,2 +1,10 @@
-insert into users (name, id) values ('joe bob', 1);
-insert into users (name, id) values ('sue bob', 2);
+insert into users (name, id) values ('alice', 1);
+insert into users (name, id) values ('bob', 2);
+insert into users (name, id) values ('charlie', 3);
+insert into users (name, id) values ('dave', 4);
+
+insert into swipes (sender, target, liked) values (1, 2, true);
+insert into swipes (sender, target, liked) values (2, 1, true);
+insert into swipes (sender, target, liked) values (3, 4, true);
+insert into swipes (sender, target, liked) values (1, 4, false);
+insert into swipes (sender, target, liked) values (4, 1, true);
